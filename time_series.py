49ff81cf7ed94665f74d6af08f170a44df5fea71
nksmkj7 <nksmkj7@gmail.com>
2024-06-08 12:02:52 +0545

    fix: final output header

--- time_series.py
+++ time_series.py
@@ -1,16 +1,21 @@
 import numpy as np
 import csv
-import sys
+import pandas as pd
 
-source_files= {
-    "ew_csv_file_path": r'source_file.EW',
-    "ns_csv_file_path": r'source_file.NS',
-    "ud_csv_file_path": r'source_file.UD',
-}
+source_files= [
+    {"source_file": r'source_file.NS','output_file': r'output_file.NS',type:'NS'},
+    {"source_file": r'source_file.UD','output_file': r'output_file.UD', type:'UD'},
+    {"source_file": r'source_file.EW','output_file': r'output_file.EW',type:'EW'},
+]
 
-print('hello nikesh ---->')
 
-sys.exit(0)
+velocity_lists={}
+
+# Define time increment and create time array from 0 to 1 with step of 0.004
+time_increment = 0.004
+time_array = np.arange(0, 40 + time_increment, time_increment)
+# Calculate scale factor SF
+
 
 # Function to read i_values from a CSV file, starting from the 18th row and reading 8 values per row
 def read_i_values_from_csv(file_path):
@@ -25,35 +30,37 @@ def read_i_values_from_csv(file_path):
                     i_values.extend(float(v) for v in split_values)
     return i_values
 
-# Read i_values from the CSV file
 
-# Calculate scale factor SF
-SF = max(i_values) / 8388608.0
-
-# Define time increment and create time array from 0 to 1 with step of 0.004
-time_increment = 0.004
-time_array = np.arange(0, 40 + time_increment, time_increment)
-
-# Ensure the length of i_values matches the length of time_array
-# If i_values has fewer elements, we will cycle through the list
-# If i_values has more elements, we will truncate the list
-i_values_extended = np.resize(i_values, time_array.shape)
-
-# Calculate velocities
-velocities = i_values_extended * SF
-
-# Write results to a new CSV file
-output_csv_file_path = r'output_file.csv'
-
-def generate_csv_file(output_csv_file_path, time_array, velocities):
+def generate_csv_file(output_csv_file_path, time_array, velocities,row_data = ['Time','Velocity']):
+    zip_data=zip(time_array,*velocities)
     with open(output_csv_file_path, mode='w', newline='') as file:
         csv_writer = csv.writer(file)
-        csv_writer.writerow(['Time', 'Velocity'])  # Write header
-        for t, v in zip(time_array, velocities):
-            csv_writer.writerow([t, v])
+        csv_writer.writerow(row_data)  # Write header
+        for t, *v in zip_data:
+            row = [t]
+            row.extend(v)
+            csv_writer.writerow(row)
     return output_csv_file_path
 
 
+for index,file_info in enumerate(source_files):
+    [source_file, output_file,type] = file_info.values()
+    i_values = read_i_values_from_csv(source_file)
+    SF = max(i_values) / 8388608.0
 
-def write_file():
-    return generate_csv_file(output_csv_file_path, time_array, velocities)
+    # Ensure the length of i_values matches the length of time_array
+    # If i_values has fewer elements, we will cycle through the list
+    # If i_values has more elements, we will truncate the list
+    i_values_extended = np.resize(i_values, time_array.shape)
+
+    # Calculate velocities
+    velocities = i_values_extended * SF
+    velocity_lists[type] = velocities  
+    generate_csv_file(output_file, time_array, [velocities])
+    
+
+generate_csv_file('final_result.csv', time_array, list(map(lambda x: list(x), velocity_lists.values())),['Time'] + list(velocity_lists.keys()))
+
+
+# def write_file():
+#     return generate_csv_file(output_csv_file_path, time_array, velocities)
